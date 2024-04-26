06cdae4159348fb50b191e321ac24573fd2617a7
Fuad Khan <fuadk@mydatacrest.com>
2024-04-26 16:05:43 -0400

    Remove rules and add action and action_type models

---
+++ app/models/action.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Action < ApplicationRecord
+  self.table_name = :nav_actions
+end
---
+++ app/models/action_type.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ActionType < ApplicationRecord
+  self.table_name = :nav_action_types
+end
