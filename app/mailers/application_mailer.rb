9df3112139151930e49f922afa822f463bc29128
Fuad Khan <fuadk@mydatacrest.com>
2024-10-25 12:53:37 -0400

    Update email host for all envs

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,12 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'noreply@mydatacrest.com'
   layout 'mailer'
+
+  helper_method :host
+
+  private
+
+  def host
+    @host ||= Rails.application.config.action_mailer.default_url_options[:host]
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.default_url_options = { host: 'navigate.mydatacrest.com' }
+  config.action_mailer.default_url_options = { host: 'https://navigate.mydatacrest.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
