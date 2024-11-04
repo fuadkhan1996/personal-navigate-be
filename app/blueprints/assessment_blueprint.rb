1ae41d4cf92e4578a1b280711148c561c5d280c0
Fuad Khan <fuadk@mydatacrest.com>
2024-11-04 10:52:30 -0500

    Fix: Change view name from :extended to :with_action_results in assessment_blueprint.rb

--- app/blueprints/assessment_blueprint.rb
+++ app/blueprints/assessment_blueprint.rb
@@ -6,7 +6,7 @@ class AssessmentBlueprint < ActivityBlueprint
   fields :id, :title, :tis_order_id, :completed_at, :status, :form_data, :deleted_at, :created_at, :updated_at
   association :account, blueprint: Dc::CompanyBlueprint, with: :with_primary_company_employee
 
-  view :extended do
+  view :with_action_results do
     association :assessment_action_results,
                 blueprint: Assessment::ActionResultBlueprint,
                 view: :with_activities do |assessment, _options|
