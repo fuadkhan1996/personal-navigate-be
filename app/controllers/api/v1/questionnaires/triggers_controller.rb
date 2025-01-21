74f2c79c10f139e252d552449195fab14d87ca88
Fuad Khan <57614199+fuadkhan1996@users.noreply.github.com>
2025-01-21 14:21:54 -0500

    Fix triggers (#57)
    * Bump rails-html-sanitizer from 1.6.0 to 1.6.1
    
    Bumps [rails-html-sanitizer](https://github.com/rails/rails-html-sanitizer) from 1.6.0 to 1.6.1.
    - [Release notes](https://github.com/rails/rails-html-sanitizer/releases)
    - [Changelog](https://github.com/rails/rails-html-sanitizer/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/rails/rails-html-sanitizer/compare/v1.6.0...v1.6.1)
    
    ---
    updated-dependencies:
    - dependency-name: rails-html-sanitizer
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump actionpack from 7.1.4.1 to 7.1.5.1 (#53)
    
    Bumps [actionpack](https://github.com/rails/rails) from 7.1.4.1 to 7.1.5.1.
    - [Release notes](https://github.com/rails/rails/releases)
    - [Changelog](https://github.com/rails/rails/blob/v8.0.0.1/actionpack/CHANGELOG.md)
    - [Commits](https://github.com/rails/rails/compare/v7.1.4.1...v7.1.5.1)
    
    ---
    updated-dependencies:
    - dependency-name: actionpack
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Update devdb host
    
    * Update URLs in emails
    
    * Add Invite bulk API (#55)
    
    * Add active_activity_triggers scoped association
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- app/controllers/api/v1/questionnaires/triggers_controller.rb
+++ app/controllers/api/v1/questionnaires/triggers_controller.rb
@@ -5,7 +5,7 @@ module Api
     module Questionnaires
       class TriggersController < ApplicationController
         def index
-          @triggers = questionnaire.activity_triggers.includes(:questionnaire, :activity)
+          @triggers = questionnaire.active_activity_triggers.includes(:questionnaire, :activity)
                                    .for_company(current_company.id).order(created_at: :desc)
 
           render json: ::Activity::TriggerBlueprint.render(@triggers, view: :extended), status: :ok
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -18,6 +18,12 @@ class Questionnaire < ApplicationRecord
            foreign_key: :nav_questionnaire_id,
            inverse_of: :questionnaire
 
+  has_many :active_activity_triggers, -> { active },
+           dependent: :restrict_with_exception,
+           class_name: 'Activity::Trigger',
+           foreign_key: :nav_questionnaire_id,
+           inverse_of: :questionnaire
+
   has_many :actions, through: :questionnaire_actions
   has_many :assessments, inverse_of: :questionnaire, dependent: :restrict_with_exception
 
