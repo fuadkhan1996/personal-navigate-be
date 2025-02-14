dfac498dfaed86ab0644151b11af7261549b31ac
Fuad Khan <fuadk@mydatacrest.com>
2025-02-14 15:55:49 -0500

    Add pinned column to associated activities db table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_14_181252) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_14_184850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -1272,6 +1272,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_14_181252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "completed_at"
+    t.boolean "pinned", default: false
     t.index ["activity_id"], name: "index_nav_associated_activities_on_activity_id"
     t.index ["assessment_id"], name: "index_nav_associated_activities_on_assessment_id"
     t.index ["company_id"], name: "index_nav_associated_activities_on_company_id"
