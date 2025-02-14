5645905411a00530dbe608eb155edb4cda0cc43e
Fuad Khan <fuadk@mydatacrest.com>
2025-02-14 15:56:38 -0500

    Add Associated Activities update API

--- spec/requests/api/v1/associated_activities_spec.rb
+++ spec/requests/api/v1/associated_activities_spec.rb
@@ -127,5 +127,63 @@ describe 'Api::V1::AssociatedActivities' do
         run_test!
       end
     end
+
+    put 'Update Associated Activity' do
+      tags 'Activities'
+      consumes 'application/json'
+      produces 'application/json'
+      security [{ bearerAuth: [], apiKeyAuth: [], xApiKey: [] }]
+
+      parameter name: :id, in: :path, type: :string
+      parameter name: :associated_activity, in: :body, schema: {
+        type: :object,
+        properties: {
+          associated_activity: {
+            type: :object,
+            properties: {
+              pinned: { type: :boolean }
+            }
+          }
+        }
+      }
+
+      response '200', 'Updated Associated Activity' do
+        schema type: :object,
+               properties: {
+                 id: { type: :string },
+                 activity: { type: :object },
+                 associated_activity_actions: { type: :array, items: { type: :object } }
+               }
+
+        run_test!
+      end
+
+      response '422', 'Unprocessable Entity' do
+        schema type: :object,
+               properties: {
+                 errors: {
+                   type: :object,
+                   additionalProperties: {
+                     type: :array,
+                     items: {
+                       type: :string
+                     }
+                   }
+                 }
+               }
+
+        run_test!
+      end
+
+      response '401', 'Unauthorized' do
+        schema type: :object,
+               properties: {
+                 error: { type: :string }
+               },
+               required: %w[error]
+
+        run_test!
+      end
+    end
   end
 end
